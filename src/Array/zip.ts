2958d473c6d93e016817a11e2816ee0bc308696b
Benji Stephenson <bjames.stephenson@gmail.com>
2024-05-15 14:41:43 +1200

    add non empty array support to array zip

--- src/Array/zip.ts
+++ src/Array/zip.ts
@@ -1,16 +1,24 @@
 import { dual } from "../functions"
 import { tuple } from "./constructors"
+import { AnyArray, ArrayElem, InferArrayType } from "./types"
 
 export const zip: {
-    <A, B>(self: A[], bs: B[]): [A, B][]
-    <B>(bs: B[]): <A>(self: A[]) => [A, B][]
+    <As extends AnyArray, Bs extends AnyArray>(self: As, bs: Bs): InferArrayType<As, [ArrayElem<As>, ArrayElem<Bs>]>
+    <Bs extends AnyArray>(bs: Bs): <As extends AnyArray>(self: As) => InferArrayType<As, [ArrayElem<As>, ArrayElem<Bs>]>
 } = dual(2, <A, B>(self: A[], bs: B[]): [A, B][] => {
     return zipWith(self, bs, tuple)
 })
 
 export const zipWith: {
-    <A, B, Out>(self: A[], bs: B[], f: (a: A, b: B) => Out): Out[]
-    <A, B, Out>(bs: B[], f: (a: A, b: B) => Out): (self: A[]) => Out[]
+    <As extends AnyArray, Bs extends AnyArray, Out>(
+        self: As,
+        bs: Bs,
+        f: (a: ArrayElem<As>, b: ArrayElem<Bs>) => Out
+    ): InferArrayType<As, Out>
+    <As extends AnyArray, Bs extends AnyArray, Out>(
+        bs: Bs,
+        f: (a: ArrayElem<As>, b: ArrayElem<Bs>) => Out
+    ): (self: As) => InferArrayType<As, Out>
 } = dual(3, <A, B, Out>(self: A[], bs: B[], f: (a: A, b: B) => Out): Out[] => {
     const len = Math.min(self.length, bs.length)
 
