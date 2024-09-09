57261844fa6abcdd180e64484c8a5e4f1d424fd6
Benji Stephenson <bjames.stephenson@gmail.com>
2024-09-09 19:56:50 +0100

    feat: deprecate groupBy, add groupOn that doesn't require sorting.

--- src/Array/groupBy.test.ts
+++ src/Array/groupBy.test.ts
@@ -1,5 +1,6 @@
 import { assertThat } from "mismatched"
-import { groupBy } from "./groupBy"
+import { groupBy, groupOn } from "./groupBy"
+import { identity } from "../functions"
 
 describe("groupBy", () => {
     it("groups a sorted array", () => {
@@ -13,7 +14,7 @@ describe("groupBy", () => {
         ])
     })
 
-    it("groups a sorted array of objects", () => {
+        it("groups a sorted array of objects", () => {
         assertThat(
             groupBy(
                 [
@@ -40,5 +41,48 @@ describe("groupBy", () => {
                 { age: 3, name: "janet" }
             ]
         ])
+    })
+})
+
+describe("groupOn", () => {
+
+    it("groups a sorted array", () => {
+        assertThat(groupOn([2, 2, 1, 2, 4, 4, 3, 6, 5, 6], identity)).is({
+            1: [1],
+            2: [2, 2, 2],
+            3: [3],
+            4: [4, 4],
+            5: [5],
+            6: [6, 6]
+        })
+    })
+
+    it("groups an array of objects", () => {
+        assertThat(
+            groupOn(
+                [
+                    { age: 3, name: "janet" },
+                    { age: 1, name: "bob" },
+                    { age: 2, name: "bob" },
+                    { age: 4, name: "janet" },
+                    { age: 3, name: "janet" },
+                    { age: 1, name: "bob" },
+                    { age: 3, name: "dave" }
+                ],
+                _ => _.age
+            )
+        ).is({
+            1: [
+                { age: 1, name: "bob" },
+                { age: 1, name: "bob" }
+            ],
+            2: [{ age: 2, name: "bob" }],
+            3: [
+                { age: 3, name: "janet" },
+                { age: 3, name: "janet" },
+                { age: 3, name: "dave" }
+            ],
+            4: [{ age: 4, name: "janet" }],
+        })
     })
 })
--- src/Array/groupBy.ts
+++ src/Array/groupBy.ts
@@ -3,6 +3,32 @@ import { dual } from "../functions"
 import { headNonEmpty } from "./destructors"
 import { isNonEmpty } from "./instance"
 
+/* Given an f that returns a string | number | symbol, group array entries that share the property
+ * returned by f into a record indexed by that value.
+ *
+ * import \{ Arr \} from "weland"
+ * Arr.groupOn([2, 2, 1, 2, 4, 4, 3, 6, 5, 6], identity) === { 1: [1], 2: [2, 2, 2], 3: [3], 4: [4, 4], 5: [5], 6: [6, 6] }
+ */
+const groupOn: {
+    <A, B extends string | number | symbol>(self: NonEmptyArray<A>, f: (a: A) => B): Record<B, NonEmptyArray<A>>
+    <A, B extends string | number | symbol>(f: (a:A ) => B): (self: NonEmptyArray<A>) => Record<B, NonEmptyArray<A>>
+} = dual(2, <A, B extends string | number | symbol>(self: NonEmptyArray<A>, f: (a: A) => B): Record<B, NonEmptyArray<A>> => {
+
+    const map: Record<B, NonEmptyArray<A>> = {} as any
+
+    for (let i = 0; i < self.length; i++) {
+        const key = f(self[i])
+        map[key] = map[key] === undefined ? [self[i]] : [...map[key], self[i]]
+    }
+
+    return map
+})
+
+
+ /* Given an equivalence function, group items where the function returns true into the same sub-array
+ * The input array *must* already be sorted.
+ * @deprecated use {@link groupOn} instead to avoid having to pre-sort
+ */
 const groupBy: {
     <A>(self: NonEmptyArray<A>, areEqual: (a: A, b: A) => boolean): NonEmptyArray<NonEmptyArray<A>>
     <A>(areEqual: (a: A, b: A) => boolean): (self: NonEmptyArray<A>) => NonEmptyArray<NonEmptyArray<A>>
@@ -26,4 +52,4 @@ const groupBy: {
     return [out, ...groupBy(rest, areEqual)]
 })
 
-export { groupBy }
+export { groupBy, groupOn }
